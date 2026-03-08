61952e3d5e002ac4be1c43d95c47f72e7611c99d
Ammarhussain50 <Hussainammar205@gmail.com>
2026-03-09 02:41:36 +0500

    Account lock functionality

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5558225c680112f070f31e00229945c25b4fa77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2b884953751c1e74a6b984e459ea3611f4d2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
